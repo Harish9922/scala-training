b80c7596ea98d2c018b729cd99fa4365b1579488
harish <bharish4321@gmail.com>
2023-12-26 18:31:13 +0530

    adding test

---
+++ src/test/scala/com/demo/ListTrainingSpec.scala
@@ -0,0 +1,17 @@
+package com.demo
+
+import org.scalatest.funsuite.AnyFunSuite
+import com.demo.ListTraining
+
+class ListTrainingSpec extends AnyFunSuite{
+
+  test(testName = "ListTrainingSpec"){
+    assert(ListTraining.multiElemBy2(List(1,2), List()) === List(2,4))
+  }
+
+  test(testName = "reversed list"){
+    assert(ListTraining.reverseList(List(1,2,3)) === List( 3, 2, 1))
+  }
+}
+
+
